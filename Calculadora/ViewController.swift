52d7fc947b91776a243f9817cdd42129e05d643f
Apps2t <apps2t@409-06.local>
2020-09-25 16:04:29 +0200

    Repositorio creado

---
+++ Calculadora/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculadora
+//
+//  Created by Apps2t on 25/09/2020.
+//  Copyright © 2020 Apps2t. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
